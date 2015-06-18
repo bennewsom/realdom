ea2b7b3b9ed2d66860894d71fd87b24ce864ead7
Shirren Premaratne <shirren.premaratne@gmail.com>
2015-06-18 20:52:39 +1000

    Class 8

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -14,4 +14,23 @@ class TenantsController < ApplicationController
       redirect_to tenants_url
     end
   end
+
+  # GET /tenants/new
+  def new
+    @tenant = Tenant.new
+  end
+
+  # POST /tenants
+  def create
+    tenant_params = params
+      .require(:tenant)
+      .permit(:name, :dob, :gender, :no_of_dependents, :annual_income, :occupation)
+    @tenant = Tenant.new(tenant_params)
+    if @tenant.save
+      flash[:notice] = 'Tenant saved, yay baby!'
+      redirect_to tenants_url
+    else
+      render :new
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :tenants, only: [:index, :show]
+  resources :tenants, only: [:index, :show, :new, :create]
 end
