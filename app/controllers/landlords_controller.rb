a222a29794b7df5d716a37a3de8c02ca6b5917f1
Shirren Premaratne <shirren.premaratne@gmail.com>
2015-06-23 22:57:28 +1000

    Fixed code from class 8

--- app/controllers/landlords_controller.rb
+++ app/controllers/landlords_controller.rb
@@ -7,7 +7,7 @@ class LandlordsController < ApplicationController
 
   # GET /landlords/:id
   def show
-    @landlord = Landlord.find(params[:id])
+    get_landlord
   end
 
   # GET /landlords/new
@@ -17,12 +17,9 @@ class LandlordsController < ApplicationController
 
   # POST /landlords
   def create
-    landlord_params = params
-      .require(:landlord)
-      .permit(:name, :dob, :gender)
-    @landlord = Landlord.new(landlord_params)
+    @landlord = Landlord.new(white_listed_parameters)
     if @landlord.save
-      flash[:notice] = 'Landlord saved, yay baby!'
+      flash[:notice] = 'Landlord saved'
       redirect_to landlords_path
     else
       render :new
@@ -31,20 +28,34 @@ class LandlordsController < ApplicationController
 
   # GET /landlords/:id/edit
   def edit
-    @landlord = Landlord.find(params[:id])
+    get_landlord
   end
 
   # PUT /landlords/:id
   def update
-    landlord_params = params
-      .require(:landlord)
-      .permit(:name, :dob, :gender)
     @landlord = Landlord.find(params[:id])
-    if @landlord.update_attributes(landlord_params)
-      flash[:notice] = 'Landlord updated, yay baby!'
+    if @landlord.update_attributes(white_listed_parameters)
+      flash[:notice] = 'Landlord saved'
       redirect_to landlords_path
     else
       render :edit
     end
+  end
+
+  private
+
+  # Get's the tenant from the database, and redirects if unable to find one
+  def get_landlord
+    begin
+      @landlord = Landlord.find(params[:id])
+    rescue
+      redirect_to landlords_path
+    end
+  end
+
+  def white_listed_parameters
+    params
+      .require(:landlord)
+      .permit(:name, :dob, :gender)
   end
 end
--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -8,11 +8,7 @@ class TenantsController < ApplicationController
 
   # GET /tenants/:id
   def show
-    begin
-      @tenant = Tenant.find(params[:id])
-    rescue
-      redirect_to tenants_url
-    end
+    get_tenant
   end
 
   # GET /tenants/new
@@ -22,13 +18,10 @@ class TenantsController < ApplicationController
 
   # POST /tenants
   def create
-    tenant_params = params
-      .require(:tenant)
-      .permit(:name, :dob, :gender, :no_of_dependents, :annual_income, :occupation)
-    @tenant = Tenant.new(tenant_params)
+    @tenant = Tenant.new(white_listed_parameters)
     if @tenant.save
-      flash[:notice] = 'Tenant saved, yay baby!'
-      redirect_to tenants_url
+      flash[:notice] = 'Tenant saved'
+      redirect_to tenants_path
     else
       render :new
     end
@@ -36,24 +29,35 @@ class TenantsController < ApplicationController
 
   # GET /tenants/:id/edit
   def edit
-    begin
-      @tenant = Tenant.find(params[:id])
-    rescue
-      redirect_to tenants_url
-    end
+    get_tenant
   end
 
   # PUT /tenants
   def update
-    tenant_params = params
-      .require(:tenant)
-      .permit(:name, :dob, :gender, :no_of_dependents, :annual_income, :occupation)
     @tenant = Tenant.find(params[:id])
-    if @tenant.update_attributes(tenant_params)
-      flash[:notice] = 'Tenant updated, yay baby!'
-      redirect_to tenants_url
+    if @tenant.update_attributes(white_listed_parameters)
+      flash[:notice] = 'Tenant saved'
+      redirect_to tenants_path
     else
       render :edit
     end
+  end
+
+  private
+
+  # Get's the tenant from the database, and redirects if unable to find one
+  def get_tenant
+    begin
+      @tenant = Tenant.find(params[:id])
+    rescue
+      redirect_to tenants_path
+    end
+  end
+
+  # White lists the corresponding parameters
+  def white_listed_parameters
+    params
+      .require(:tenant)
+      .permit(:name, :dob, :gender, :no_of_dependents, :annual_income, :occupation)
   end
 end
