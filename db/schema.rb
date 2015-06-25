80ede7ed2672ced3b531a9ae1b96964b0c226e65
Shirren Premaratne <shirren.premaratne@gmail.com>
2015-06-25 15:39:23 +1000

    Rolled back code to where we were last Thursday

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150623125922) do
-
-  create_table "countries", force: :cascade do |t|
-    t.string   "name",       limit: 512, null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
+ActiveRecord::Schema.define(version: 20150616085932) do
 
   create_table "landlords", force: :cascade do |t|
     t.string   "name",       null: false
@@ -26,15 +20,6 @@ ActiveRecord::Schema.define(version: 20150623125922) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "states", force: :cascade do |t|
-    t.string   "name",       limit: 512, null: false
-    t.integer  "country_id",             null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-  end
-
-  add_index "states", ["country_id"], name: "index_states_on_country_id"
 
   create_table "tenants", force: :cascade do |t|
     t.string   "name",             null: false
