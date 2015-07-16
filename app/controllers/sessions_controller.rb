872a570b14fd062464fa883f86dbc28a489d13fe
Shirren Premaratne <shirren.premaratne@gmail.com>
2015-07-16 14:23:04 +1000

    Need to fix Realdom, please do not pull this version

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,3 @@
 class SessionsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:show, :new, :create, :edit, :update, :destroy]
 end
